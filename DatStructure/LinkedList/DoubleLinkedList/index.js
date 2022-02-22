5bbde1be034bdc2846d55979d278b552a103cb65
prakash2812 <pltkval@gmail.com>
2022-02-22 20:24:06 +0530

    linked list update

---
+++ DatStructure/LinkedList/DoubleLinkedList/index.js
@@ -0,0 +1,124 @@
+// node - is an object have {value:2,next:nextNode,prev:prevNode}
+// linkedList ---- list of elements linked/connected together in a file. it has
+// { head: { { value: 2, next: null, prev: null } }, tail: { { value: 2, next: null, prev: null } } }
+
+// single linked list - each node has reference to the node after it or the next node
+
+// double linked list - each node has reference to the next node and also it has reference to the one before it
+
+function DoubleLinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+// void functions , imagine the images and change the prev to newNode and if not exits any head then assign fresh to new node
+// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
+DoubleLinkedList.prototype.addToHead = function (value) {
+    let newNode = new Node(value, this.head, null);
+    if (this.head) {
+        this.head.prev = newNode;
+    } else {
+        this.tail = newNode;
+    }
+    this.head = newNode;
+};
+
+// void functions , imagine the images and change the next to newNode and if not exits any tails then assign fresh to new node
+// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
+DoubleLinkedList.prototype.addToTail = function (value) {
+    let newNode = new Node(value, null, this.tail);
+    if (this.tail) {
+        this.tail.next = newNode;
+    } else {
+        this.head = newNode;
+    }
+    this.tail = newNode;
+};
+
+//  return removed value
+// all we have to do is change the head//tails respective to next/prev
+// check if node exits or not
+//  if exists then again one node exists and more exists
+// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
+
+DoubleLinkedList.prototype.removeHead = function () {
+    if (!this.head) return null;
+    let value = this.head.value;
+    this.head = this.head.next;
+    if (this.head) {
+        this.head.prev = null;
+    } else {
+        this.tail = null;
+    }
+    return value;
+};
+
+//  return removed value
+// all we have to do is change the head//tails respective to next/prev
+// check if node exits or not
+//  if exists then again one node exists and more exists
+// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
+DoubleLinkedList.prototype.removeTail = function () {
+    if (!this.tail) return null;
+    let value = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) {
+        this.tail.next = null;
+    } else {
+        this.head = null;
+    }
+    return value;
+};
+
+// input value can be number or string
+//if not exists output return null or if exits return value
+// linear search runtime o(n) -- based on input increases search functionality execution time increases proportionally
+DoubleLinkedList.prototype.search = function (value) {
+    // we can do either from head or tail
+    let currentNode = this.head;
+    while (currentNode) {
+        if (currentNode.value === value) {
+            return value;
+        }
+        currentNode = currentNode.next;
+    }
+    return null;
+};
+
+// input value - 3->5->3->8
+//output [0,2] indexes of given values
+//  remember linked list is all elements are in linked each other so we have to give our count variable from 0
+DoubleLinkedList.prototype.indexOf = function (value) {
+    // we can do either from head or tail
+    let indexes = [];
+    let currentNode = this.head;
+    let countIndex = 0;
+    while (currentNode) {
+        if (currentNode.value === value) {
+            indexes.push(countIndex);
+        }
+        currentNode = currentNode.next;
+        countIndex++;
+    }
+    return indexes;
+};
+
+let list = new DoubleLinkedList();
+
+list.addToHead(10);
+list.addToHead(20);
+list.addToHead(30);
+list.addToHead(40);
+list.addToTail(50);
+list.addToTail(60);
+console.log(list.removeHead());
+console.log(list.removeTail());
+console.log(list);
+console.log(list.search(20));
+console.log(list.indexOf(20));
--- DatStructure/LinkedList/index.js
+++ DatStructure/LinkedList/index.js
@@ -1,124 +1,70 @@
-// node - is an object have {value:2,next:nextNode,prev:prevNode}
-// linkedList ---- list of elements linked/connected together in a file. it has
-// { head: { { value: 2, next: null, prev: null } }, tail: { { value: 2, next: null, prev: null } } }
-
 // single linked list - each node has reference to the node after it or the next node
-
-// double linked list - each node has reference to the next node and also it has reference to the one before it
 
 function LinkedList() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
 }
 
-function Node(value, next, prev) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
+function Node(value, next) {
+    this.value = value || null;
+    this.next = next || null;
 }
 
-// void functions , imagine the images and change the prev to newNode and if not exits any head then assign fresh to new node
-// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
-LinkedList.prototype.addToHead = function (value) {
-    let newNode = new Node(value, this.head, null);
-    if (this.head) {
-        this.head.prev = newNode;
-    } else {
-        this.tail = newNode;
-    }
-    this.head = newNode;
-};
-
-// void functions , imagine the images and change the next to newNode and if not exits any tails then assign fresh to new node
-// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
-LinkedList.prototype.addToTail = function (value) {
-    let newNode = new Node(value, null, this.tail);
-    if (this.tail) {
-        this.tail.next = newNode;
-    } else {
+// add to tail
+// return length of linked list
+LinkedList.prototype.push = function (value) {
+    let newNode = new Node(value);
+    if (!this.tail) {
         this.head = newNode;
+    } else {
+        this.tail.next = newNode;
     }
     this.tail = newNode;
+    return ++this.length;
 };
 
-//  return removed value
-// all we have to do is change the head//tails respective to next/prev
-// check if node exits or not
-//  if exists then again one node exists and more exists
-// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
+//ADD HEAD
+//return length of linked list
+LinkedList.prototype.unshift = function (value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+        this.tail = newNode;
+    } else {
+        newNode.next = this.head;
+    }
+    this.head = newNode;
+    ++this.length;
+};
 
-LinkedList.prototype.removeHead = function () {
+// remove head
+// return the removed value of head
+LinkedList.prototype.shift = function () {
     if (!this.head) return null;
     let value = this.head.value;
     this.head = this.head.next;
-    if (this.head) {
-        this.head.prev = null;
-    } else {
+    if (!this.head) {
         this.tail = null;
     }
+    --this.length;
     return value;
 };
 
-//  return removed value
-// all we have to do is change the head//tails respective to next/prev
-// check if node exits or not
-//  if exists then again one node exists and more exists
-// constant runtime o(1) --> even input increase also it wont effect since it have head and tail reference easy to identify.
-LinkedList.prototype.removeTail = function () {
+// remove Tail
+// return the removed value of tail
+LinkedList.prototype.pop = function () {
     if (!this.tail) return null;
     let value = this.tail.value;
-    this.tail = this.tail.prev;
-    if (this.tail) {
-        this.tail.next = null;
-    } else {
-        this.head = null;
-    }
-    return value;
-};
-
-// input value can be number or string
-//if not exists output return null or if exits return value
-// linear search runtime o(n) -- based on input increases search functionality execution time increases proportionally
-LinkedList.prototype.search = function (value) {
-    // we can do either from head or tail
-    let currentNode = this.head;
-    while (currentNode) {
-        if (currentNode.value === value) {
-            return value;
+    let current = this.head;
+    while (current) {
+        if (current.next) {
         }
-        currentNode = currentNode.next;
     }
-    return null;
 };
-
-// input value - 3->5->3->8
-//output [0,2] indexes of given values
-//  remember linked list is all elements are in linked each other so we have to give our count variable from 0
-LinkedList.prototype.indexOf = function (value) {
-    // we can do either from head or tail
-    let indexes = [];
-    let currentNode = this.head;
-    let countIndex = 0;
-    while (currentNode) {
-        if (currentNode.value === value) {
-            indexes.push(countIndex);
-        }
-        currentNode = currentNode.next;
-        countIndex++;
-    }
-    return indexes;
-};
-
-let list = new LinkedList();
-
-list.addToHead(10);
-list.addToHead(20);
-list.addToHead(30);
-list.addToHead(40);
-list.addToTail(50);
-list.addToTail(60);
-console.log(list.removeHead());
-console.log(list.removeTail());
-console.log(list);
-console.log(list.search(20));
-console.log(list.indexOf(20));
+let ll = new LinkedList();
+ll.push(123);
+console.log('Linked list data', ll);
+// ll.push(23);
+// ll.push(13);
+ll.shift();
+console.log('Linked list data', ll);
